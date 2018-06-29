7210e029f09087e1471c4d78864a4a20db83bbb0
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-29 01:19:11 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+  def initialize(name, breed = "Mutt")
+    @name = name
+
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+  
+end
